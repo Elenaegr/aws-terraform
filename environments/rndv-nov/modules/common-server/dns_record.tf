9d6b21b3106f6dae9e3aa2e085b0d0132d63f1fa
Sergey Izgiyaev <sergo27@gmail.com>
2017-03-07 19:22:29 +0200

    .

--- environments/rndv-nov/modules/common-server/dns_record.tf
+++ environments/rndv-nov/modules/common-server/dns_record.tf
@@ -7,3 +7,14 @@ resource "powerdns_record" "srv_fqdn" {
   ttl        = 600
   records    = ["1.1.1.1"]
 }
+
+#resource "powerdns_record" "srv_fqdn" {
+#  depends_on = ["vsphere_virtual_machine.srv"]
+#  count      = 1 #"${length(vsphere_virtual_machine.srv)}"
+#  zone       = "${var.dns_domain}"
+# name       = "${format("%s.%s", vsphere_virtual_machine.srv[count.index].name, var.dns_domain)}"
+#type       = "A"
+#ttl        = 600
+#records    = ["${vsphere_virtual_machine.srv[count.index].network_interface.0.ipv4_address}"]
+#}
+
