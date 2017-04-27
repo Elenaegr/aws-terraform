920edca4251b7417fdbad7438ef9a486f3080758
abf <alberto.baselgafernandez@telefonica.com>
2017-04-27 13:30:18 +0200

    Include DNS wildcard for Ingress Controller traffic

--- environments/dev.ran/kubernetes_dns.tf
+++ environments/dev.ran/kubernetes_dns.tf
@@ -14,3 +14,11 @@ resource "powerdns_record" "k8s_svc_wildcard" {
   ttl     = 600
   records = ["${powerdns_record.k8s_svc.name}"]
 }
+
+resource "powerdns_record" "k8s_ingress_wildcard" {
+  zone    = "${lower(format("%s.", var.env_domain))}"
+  name    = "${format("*.ing.k8s.%s.", var.env_domain)}"
+  type    = "CNAME"
+  ttl     = 600
+  records = ["${format("kubworker.%s", var.env_domain)}"]
+}
